6e965ef1a703e5ebf5cda444211e2bbb14d0f3e2
Leo <88931222+JrDevLeo@users.noreply.github.com>
2022-10-28 07:50:56 +0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-token = "TOKEN" #token
-footer = "LEO4BEY 💖 Nocturia" # footer ve oynuyor kısmı - footer and bot activity status
-log_ch = 1031272416904749106 #log kanal id
-admin = 1031267213853863966 # admin rol id
-servernm = "Nocturia" # Sunucu adı
